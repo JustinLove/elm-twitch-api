0502ea12d00315bfba0bc0e32d242a0e17bc081c
Justin Love <git@JustinLove.name>
2018-09-17 11:07:38 -0500

    provide parsing context for error messages

--- src/Twitch/Parse.elm
+++ src/Twitch/Parse.elm
@@ -1,47 +1,52 @@
 module Twitch.Parse exposing (duration, int)
 
-import Parser exposing (..)
---import Parser.Advanced exposing (inContext)
+import Parser.Advanced exposing (..)
 import Char
 
-duration : Parser Int
+type alias ContextParser a = Parser String String a
+
+duration : ContextParser Int
 duration =
-  succeed (\i f -> (f i) * 1000)
-    |= int
-    |= oneOf
-      [ hxxmxxs
-      , mxxs
-      , s
-      ]
+  inContext "parsing a duration" <|
+    succeed (\i f -> (f i) * 1000)
+      |= int
+      |= oneOf
+        [ hxxmxxs
+        , mxxs
+        , s
+        ]
 
-hxxmxxs : Parser (Int -> Int)
+hxxmxxs : ContextParser (Int -> Int)
 hxxmxxs =
-  succeed (\m s_ h -> h*60*60 + m*60 + s_)
-    |. symbol "h"
-    |= int
-    |. symbol "m"
-    |= int
-    |. symbol "s"
+  inContext "Trying hours-minutes-seconds duration" <|
+    succeed (\m s_ h -> h*60*60 + m*60 + s_)
+      |. symbol (Token "h" "Looking for 'h'")
+      |= int
+      |. symbol (Token "m" "Expecting 'm'")
+      |= int
+      |. symbol (Token "s" "Final 's'")
 
-mxxs : Parser (Int -> Int)
+mxxs : ContextParser (Int -> Int)
 mxxs =
-  succeed (\s_ m -> m*60 + s_)
-    |. symbol "m"
-    |= int
-    |. symbol "s"
+  inContext "Trying minutes-seconds duration" <|
+    succeed (\s_ m -> m*60 + s_)
+      |. symbol (Token "m" "Looking for 'm'")
+      |= int
+      |. symbol (Token "s" "Final 's'")
 
-s : Parser (Int -> Int)
+s : ContextParser (Int -> Int)
 s =
-  succeed (\s_ -> s_)
-    |. symbol "s"
+  inContext "Trying seconds only duration" <|
+    succeed (\s_ -> s_)
+      |. symbol (Token "s" "Looking for 's'")
 
-int : Parser Int
+int : ContextParser Int
 int =
-  --inContext "int" <|
+  inContext "parsing int" <|
     (getChompedString (chompWhile Char.isDigit)
       |> andThen (\str -> case String.toInt str of
         Just i -> succeed i
-        Nothing -> problem ("String.toInt failed on " ++ str)
+        Nothing -> problem ("String.toInt failed on (" ++ str ++ ")")
       )
     )
 
--- test/Parser.elm
+++ test/Parser.elm
@@ -5,7 +5,7 @@ import Test exposing (it, describe, Test)
 import Runner exposing (runAll)
 
 import Html exposing (Html)
-import Parser exposing ((|.))
+import Parser.Advanced as Parser exposing ((|.))
 
 main : Html msg
 main =
@@ -15,13 +15,13 @@ all : Test
 all = describe "parsering"
   [ describe "builtin functions"
     [ it "int" <|
-      eql (Ok 11) (Parser.run Parser.int "11")
+      eql (Ok 11) (Parser.run parserInt "11")
     , it "does handle suffixed int" <|
-      case (Parser.run Parser.int "11h22m33s") of
+      case (Parser.run parserInt "11h22m33s") of
         Err err -> let _ = Debug.log "err" err in isTrue False
         Ok i -> eql 11 i
     , it "does not handle leading 0" <|
-      case (Parser.run Parser.int "02m3s") of
+      case (Parser.run parserInt "02m3s") of
         Err err -> let _ = Debug.log "err" err in isTrue False
         Ok i -> eql 0 i -- we want 2
     ]
@@ -31,7 +31,7 @@ all = describe "parsering"
     , it "suffixed int" <|
       eql (Ok 11) (Parser.run int "11h")
     , it "suffixed int and suffix" <|
-      eql (Ok 11) (Parser.run (int |. Parser.symbol "h") "11h")
+      eql (Ok 11) (Parser.run (int |. Parser.symbol (Parser.Token "h" "")) "11h")
     , it "3 element duration" <|
       eql
         (Ok ((60 * 60 * 11 + 60 * 2 + 3) * 1000))
@@ -52,5 +52,11 @@ all = describe "parsering"
       case (Parser.run duration "s") of
         Err err -> let _ = Debug.log "err" err in isTrue True
         Ok _ -> isTrue False
+    , it "ignores invalid input" <|
+      case (Parser.run duration "3h39s") of
+        Err err -> let _ = Debug.log "err" err in isTrue True
+        Ok _ -> isTrue False
     ]
   ]
+
+parserInt = Parser.int "expecting int" "invalid number"
